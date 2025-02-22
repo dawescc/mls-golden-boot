c4b662f73039b51141dc6dc436e3bd94b811f6a5
Ryan Dawes <dev@dawes.cc>
2025-02-22 18:55:09 -0500

    Version 1.1.10 Released + Corrected bug that caused load states to behave unexpectedly

--- components/teams-leaderboard.tsx
+++ components/teams-leaderboard.tsx
@@ -89,7 +89,7 @@ const LeaderboardLoading = () => {
 	return (
 		<div className='flex w-full flex-col overflow-hidden shadow-sm border border-layer-5/70 bg-layer-0 rounded'>
 			<div className='flex flex-grow flex-row items-center justify-between p-3 bg-layer-2'>
-				<p className='text-xl font-medium'>Leaderboard</p>
+				<p className='text-lg font-medium text-accent'>Owner</p>
 				<div className='p-1 border border-layer-5/80 rounded-full bg-layer-3'>
 					<CgDetailsMore className='size-6 text-accent' />
 				</div>
